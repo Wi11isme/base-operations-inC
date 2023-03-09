4ed3130bb306892e2130b7cd344a91983b3b1763
Willis Melodi <willisme@et-b1.kzn.21-school.ru>
2023-03-09 21:16:33 +0300

    quest3

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 
 int sum(int a, int b);
+int dif(int a, int b);
+int prod(int a, int b);
+int quot(int a, int b);
 
-int main()
-{
+int main() {
+    double first_count, second_count;
+    if (scanf("%lf", &first_count) == 0) {
+        printf("n/a\n");
+        return 0;
+    } else if (scanf("%lf", &second_count) == 0) {
+        printf("n/a\n");
+        return 0;
+    }
+    if (first_count != (int)first_count || second_count != (int)second_count) {
+        printf("n/a\n");
+        return 0;
+    }
+
+    if (second_count == 0) {
+        printf("%d %d %d n/a\n", sum(first_count, second_count), dif(first_count, second_count),
+               prod(first_count, second_count));
+    } else
+        printf("%d %d %d %d\n", sum(first_count, second_count), dif(first_count, second_count),
+               prod(first_count, second_count), quot(first_count, second_count));
     return 0;
 }
 
-int sum(int a, int b){
-    return (2 * a * b) / (a - b);
-}
+int sum(int a, int b) { return a + b; }
+
+int dif(int a, int b) { return a - b; }
+
+int prod(int a, int b) { return a * b; }
+
+int quot(int a, int b) { return a / b; }
