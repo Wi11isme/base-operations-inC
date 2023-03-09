7278fa15da5de8e8fba7dfa18788fe21496de366
Willis Melodi <willisme@et-b1.kzn.21-school.ru>
2023-03-09 22:54:54 +0300

    quest7

---
+++ src/crack.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    double x, y;
+    if (scanf("%lf", &x)==0)
+    {
+        printf("n/a\n");
+        return 0;
+    }
+    else if (scanf("%lf", &y)==0)
+    {
+        printf("n/a\n");
+        return 0;
+    }
+    if (x*x + y*y <=25) printf("GOTCHA\n");
+    else printf("MISS\n");
+    return 0;
+}
